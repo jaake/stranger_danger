9d6bbeac3c4ce356ac97b9b9b5d84b2ec2f9e715
jaake <jmargason758@gmail.com>
2014-09-13 14:43:49 -0500

    add loader animation, add timeout

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,23 +1,32 @@
 
 $(document).ready(function() {
   getLocation();
-  setInterval(function(){getLocation()}, 25000);
-  setInterval(function(){auto_push()}, 3000);
+  setInterval(function(){getLocation()}, 30000);
+  setInterval(function(){auto_push()}, 2000);
   audio.play();
+  $('#loader').hide();
 });
 
 var x = document.getElementById("location");
-
 var audio = new Audio('stranger.mp3');
+var log = 0;
 
-
-$('#image_photo').on('change', auto_push());
+//$('#image_photo').on('change', auto_push());
 
 function auto_push() {
     if ($('#image_photo').val() != "") {
         $("#tag").submit();
-    }else {
-
+        $("#photo").hide();
+        $("#loader").show();
+        log = 0
+    } else {
+        log = log + 1;
+        if (log > 14) {
+            $('#loader').hide();
+            $('#photo').show();
+        } else {
+            //console.log();
+        }
     }
 }
 
@@ -38,19 +47,5 @@ function pushPosition(position) {
     })
 }
 
-function pubLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(publishImage);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function publishImage(position) {
-	$.ajax('publish.js', {
-      format: 'js',
-      data: {latitude: position.coords.latitude, longitude: position.coords.longitude}
-    })
-}
 
 
